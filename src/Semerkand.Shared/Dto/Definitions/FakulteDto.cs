25a3f612b96d2e828c1fdc38420f8d8ffdfdd8d8
ahmetsekmen <ahmet.sekmen@hotmail.com>
2020-05-11 05:56:28 +0300

    Color fix. Ekranlar

--- src/Semerkand.Shared/Dto/Definitions/FakulteDto.cs
+++ src/Semerkand.Shared/Dto/Definitions/FakulteDto.cs
@@ -6,31 +6,40 @@ namespace Semerkand.Shared.Dto.Definitions
 {
     public class FakulteDto : EntityDto<int>
     {
-        //[Key]
-        //public int Id { get; set; }
-
         [Required]
         [MaxLength(300)]
         public string Ad { get; set; }
-
-
+        public int Kod { get; set; }
         public int UniversiteId { get; set; }
-        public virtual UniversiteDto Universite { get; set; }
+        public virtual Universite Universite { get; set; }
 
-        //[System.ComponentModel.DataAnnotations.Schema.NotMapped]
-        //public string UniversiteAd
-        //{
-        //    get
-        //    {
-        //        return Universite.Ad;
-        //    }
-        //    set
-        //    {
-        //        Universite.Ad = value;
-        //    }
-        //}
+        public int FakulteTurId { get; set; }
+        public virtual FakulteTur FakulteTur { get; set; }
+
+        public string KisaAd { get; set; }
+
+        public string AdEn { get; set; }
+        public string EPosta { get; set; }
+        public string Tel { get; set; }
+        public string Adres { get; set; }
+        public string Adres2 { get; set; }
+        public string Faks { get; set; }
+        public string Web { get; set; }
+        public int IlceId { get; set; }
+        public int OgrenimTurId { get; set; }
+        public virtual OgrenimTur OgrenimTur { get; set; }
+        public int OgrenimSure { get; set; }
+        public int IlKod { get; set; }
+        public int Tip { get; set; }
+        public string DiplomaAd { get; set; }
+        public bool IsBologna { get; set; }
+        public string BolognaIcerikTR { get; set; }
+        public string BolognaIcerikEN { get; set; }
+        public int BirimID { get; set; }
 
 
-        public ICollection<BolumDto> Bolums { get; set; }
+        public virtual ICollection<Bolum> Bolums { get; set; }
+
+        public virtual ICollection<Ogrenci> Ogrencis { get; set; }
     }
 }
