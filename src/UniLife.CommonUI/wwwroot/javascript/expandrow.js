3d318ebce7d89703e537ee3517b4874a827dc02d
ahmetsekmen <ahmet.sekmen@hotmail.com>
2020-07-13 20:48:43 +0300

    fix

--- src/UniLife.CommonUI/wwwroot/javascript/expandrow.js
+++ src/UniLife.CommonUI/wwwroot/javascript/expandrow.js
@@ -1,9 +1,10 @@
 ﻿var prevrowIndex;
 document.addEventListener('click', function (args) {
     if (args.target.classList.contains("e-dtdiagonalright") || args.target.classList.contains("e-detailrowcollapse")) {
-        var gObj = document.getElementById("OgrGrid").ej2_instances[0];
+        var gObj = document.getElementsByClassName("e-grid")[0].blazor__instance;
         if (prevrowIndex != undefined) {
-            gObj.detailRowModule.collapse(prevrowIndex);
+            var rowele = gObj.getRowByIndex(prevrowIndex).querySelector(".e-detailrowexpand");
+            rowele.click();
         }
         prevrowIndex = parseInt(args.target.closest("tr").getAttribute("aria-rowindex"));
     }
