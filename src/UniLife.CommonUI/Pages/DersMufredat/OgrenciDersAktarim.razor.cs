154130f4beb9d885d2bb9ea588167da88c2bcba1
ahmetsekmen <ahmet.sekmen@hotmail.com>
2020-09-04 01:17:43 +0300

    örg dres atama

---
+++ src/UniLife.CommonUI/Pages/DersMufredat/OgrenciDersAktarim.razor.cs
@@ -0,0 +1,163 @@
+﻿using MatBlazor;
+using Microsoft.AspNetCore.Components;
+using Syncfusion.Blazor.Data;
+using Syncfusion.Blazor.DropDowns;
+using Syncfusion.Blazor.Grids;
+using Syncfusion.Blazor.Navigations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using UniLife.CommonUI.Extensions;
+using UniLife.Shared.Dto;
+using UniLife.Shared.Dto.Definitions;
+
+namespace UniLife.CommonUI.Pages.DersMufredat
+{
+    public partial class OgrenciDersAktarim : ComponentBase
+    {
+        [InjectAttribute]
+        public System.Net.Http.HttpClient Http { get; set; }
+        [InjectAttribute]
+        public MatBlazor.IMatToaster matToaster { get; set; }
+
+        public DersAcilanDto _dersAcilanDto { get; set; } = new DersAcilanDto();
+
+        SfDropDownList<int?, KeyValueDto> DropDers2;
+        SfDropDownList<int?, KeyValueDto> DropDers;
+
+        SfDropDownList<int?, KeyValueDto> DropDonem2;
+        SfDropDownList<int?, KeyValueDto> DropDonem;
+        SfDropDownList<int?, KeyValueDto> DropFakulte;
+        SfDropDownList<int?, KeyValueDto> DropBolum;
+        SfDropDownList<int?, KeyValueDto> DropProgram;
+
+        public Query donemQuery = new Query().Select(new List<string> { "Id", "Ad" }).RequiresCount();
+
+        string OdataDersKaynakQuery;//= $"odata/dersacilans?$filter=programId eq {ProgramId}";
+        //public Query dersQuery = new Query()
+        //    .Expand(new List<string> { "program($select=Id,Ad)", "Akademisyen($select=Id,Ad)", "Donem($select=Id,Ad)", "bolum($expand=fakulte($select=Ad,Id);$select=Ad,Id)" })
+        //    .Select(new List<string> { "Id", "Ad" }).RequiresCount();
+        public Query ders2Query = new Query().Select(new List<string> { "Id", "Ad" }).RequiresCount();
+
+        int? _programId;
+        //public int? ProgramId
+        //{
+        //    get => _programId;
+        //    set
+        //    {
+        //        if (_programId.HasValue)
+        //        {
+        //            OdataDersKaynakQuery = $"odata/dersacilans?$filter=programId eq {_programId}";
+        //            DropDers.Refresh();
+        //        }
+        //        else
+        //        {
+        //            OdataDersKaynakQuery = null;
+        //            //DropDers.DataSource = null;
+        //            DropDers.Clear();
+        //            DropDers.Refresh();
+        //        }
+        //        //StateHasChanged();
+        //        base.InvokeAsync(StateHasChanged);
+        //        //DropDers.Refresh();
+        //        if (_programId == value) return;
+        //        _programId = value;
+        //    }
+        //}
+
+        int? bolumId;
+        int? fakulteId;
+        int? donemId;
+        int? dersId;
+        int? sinif;
+
+        int? _programId2;
+        int? bolumId2;
+        int? fakulteId2;
+        int? sinif2;
+        int? donemId2;
+        int? dersId2;
+
+        bool aktarimConfirmDialogOpen;
+        string aktarimConfirmDialogTitle="Öğrenci Ders Aktarımı";
+
+        //List<SinifDto> sinifDtos = SinifDto.SinifDtos;
+        SfDropDownList<int?, SinifDto> DropSinif;
+        SfDropDownList<int?, SinifDto> DropSinif2;
+
+        SfGrid<DersKayitDto> ogrGrid;
+        SfGrid<DersKayitDto> ogrHedefGrid;
+        
+        bool kaynakVisible=true;
+        bool hedefVisible = true;
+        protected async override Task OnInitializedAsync()
+        {
+
+        }
+        
+
+        async Task AktarimYap()
+        {
+
+        }
+
+        async Task KaynakChange()
+        {
+            kaynakVisible = false;
+            await Task.Delay(100);
+            kaynakVisible = true;
+        }
+
+        async Task HedefChange()
+        {
+            hedefVisible = false;
+            await Task.Delay(100);
+            hedefVisible = true;
+        }
+
+        async Task ProgramIdChangedHandler(int? programId)
+        {
+            if (programId.HasValue)
+            {
+                kaynakVisible = false;
+                _programId = null;
+                dersId = null;
+                await Task.Delay(100);
+                _programId = programId;
+                kaynakVisible = true;
+            }
+            else
+            {
+                _programId = null;
+                dersId = null;
+                kaynakVisible = false;
+                await Task.Delay(100);
+                kaynakVisible = true;
+            }
+        }
+        async Task ProgramId2ChangedHandler(int? programId2)
+        {
+            if (programId2.HasValue)
+            {
+                hedefVisible = false;
+                _programId2 = null;
+                dersId2 = null;
+                await Task.Delay(100);
+                _programId2 = programId2;
+                hedefVisible = true;
+            }
+            else
+            {
+                _programId2 = null;
+                dersId2 = null;
+                hedefVisible = false;
+                await Task.Delay(100);
+                hedefVisible = true;
+            }
+        }
+        
+
+    }
+}
--- src/UniLife.Shared/Dto/Definitions/SinifDto.cs
+++ src/UniLife.Shared/Dto/Definitions/SinifDto.cs
@@ -9,5 +9,19 @@ namespace UniLife.Shared.Dto.Definitions
     public class SinifDto : EntityDto<int?>
     {
         public string Ad { get; set; }
+
+        public static List<SinifDto> SinifDtos { get;} = new List<SinifDto>
+        {
+            new SinifDto() { Ad = "Haz.", Id = 0 },
+            new SinifDto() { Ad = "1", Id = 1 },
+            new SinifDto() { Ad = "2", Id = 2 },
+            new SinifDto() { Ad = "3", Id = 3 },
+            new SinifDto() { Ad = "4", Id = 4 },
+            new SinifDto() { Ad = "5", Id = 5 },
+            new SinifDto() { Ad = "6", Id = 6 },
+            new SinifDto() { Ad = "7", Id = 7 },
+            new SinifDto() { Ad = "8", Id = 8 },
+            new SinifDto() { Ad = "9", Id = 9 },
+        };
     }
 }
